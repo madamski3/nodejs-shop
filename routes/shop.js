1636d9db104656b47c6445ec8062bd7d0fe2a977
Michael Adamski <m.adamski3@gmail.com>
2023-01-22 10:50:28 -0600

    Store added product data in array that can be shared between files

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const express = require("express");
 
+const adminData = require("./admin");
 const rootDir = require("../util/path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
